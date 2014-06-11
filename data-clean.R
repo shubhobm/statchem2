0cc0f89e15ec05d7695ae80c2e6e7ab58c1c4399
shubhobm <zoom.subha@gmail.com>
2014-06-11 13:06:40 -0500

    .

--- data-clean.R
+++ data-clean.R
@@ -5,3 +5,14 @@ lta98 = read.csv("lta98.csv", header=T)
 cats = factor(as.numeric(lta98[1,]))
 lta = lta98[,which(cats!=0)]
 Y = lta98[,which(colnames(lta98)=='ACT')]
+
+## separate different variable types
+ltaTS = lta98[,which(cats==1)]
+ltaTC = lta98[,which(cats==2)]
+lta3D = lta98[,which(cats==3)]
+ltaQC = lta98[,which(cats==4)]
+
+lta98 = list(Y=Y,ltaTS=ltaTS,ltaTC=ltaTC,lta3D=lta3D,ltaQC=ltaQC)
+save(lta98, file='lta98.rda')
+
+# load('lta98.rda')
